d534e5f45fa50ccf3da2bce0a9ae6a0134dbe70c
Umair Saleem <saleemakhtar35302@gmail.com>
2023-09-30 17:02:44 +0500

    setup express server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export function hello(username: string): void {
-  console.log(`Hello, ${username}`);
-}
+import { app } from './app';
+
+const PORT = 8000;
+
+// fake DB connection
+const connectToDB = () => {
+  return Promise.resolve();
+};
+
+const startServer = async () => {
+  // establish DB connection here
+  await connectToDB();
+
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT} 🚀`));
+};
+
+startServer().catch((err) => {
+  console.error(err instanceof Error ? err.message : err);
+});
