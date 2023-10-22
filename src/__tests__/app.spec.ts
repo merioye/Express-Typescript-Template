8e721f6107ad1e33c0fa5cd24917c129fae4cd66
Umair Saleem <saleemakhtar35302@gmail.com>
2023-10-22 19:58:53 +0500

    refactor test setup

--- src/__tests__/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest';
-import { app } from '../app';
-
-describe('App', () => {
-  it('returns 200 status', async () => {
-    const response = await request(app).get('/api').send();
-    expect(response.statusCode).toBe(200);
-  });
-});
