e5f31a9279744126c8f0bc503cfaa8a21212d90f
Umair Saleem <saleemakhtar35302@gmail.com>
2023-10-22 21:20:26 +0500

    refactor routes setup

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response, json } from 'express';
 import helmet from 'helmet';
 import compression from 'compression';
 import createError from 'http-errors';
-import { router } from './routes';
+import { authRouter } from './routes';
 import { errorHandlerMiddleware } from './middlewares';
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(helmet());
 app.use(compression());
 app.use(json());
 
-app.use('/api', router);
+app.use('/api/v1/auth', authRouter);
 
 // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 app.all('*', (req: Request, res: Response): never => {
